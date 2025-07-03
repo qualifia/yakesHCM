78dfed713c4d199cf45cc54aedf6b51002ca397b
rafipratama368 <rafigunadi368@gmail.com>
2025-07-03 17:02:51 +0700

    Update 2025_07_03_035938_add_fields_to_users_table.php

--- database/migrations/2025_07_03_035938_add_fields_to_users_table.php
+++ database/migrations/2025_07_03_035938_add_fields_to_users_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('username')->unique()->nullable();
-            $table->string('role')->default('Guest');
             $table->boolean('active')->default(true);
         });
     }
