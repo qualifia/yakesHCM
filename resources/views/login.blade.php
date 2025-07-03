641cb341e80895071ea69a81f0b26a6ea246a607
lifiaa298 <quaaxy@gmail.com>
2025-07-03 15:50:22 +0700

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,11 +6,58 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login - Aplikasi Sistem HCM Yayasan Kesehatan Telkom</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+
+    <style>
+        body, html {
+            font-family: 'Poppins', sans-serif;
+            font-size: 25px;
+            height: 100%;
+            margin: 0;
+        }
+
+        .center-wrapper {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+        }
+
+        .form-box {
+            width: 100%;
+            max-width: 700px;
+        }
+
+        .page-title {
+            font-size: 32px;
+            font-weight: 700;
+            text-align: center;
+            margin-bottom: 20px;
+            word-wrap: break-word;
+        }
+
+        .form-control {
+            font-size: 20px;
+            height: 60px;        /* Memperbesar tinggi box */
+            padding: 15px 20px;  /* Menambah ruang dalam box */
+            border-radius: 10px;
+        }
+
+        .custom-btn {
+            height: 70px;                 /* Tinggi tombol */
+            font-size: 22px;              /* Ukuran font */
+            display: flex;
+            align-items: center;
+            justify-content: center;     /* Horizontal center */
+            border-radius: 10px;
+        }
+
+    </style>
 </head>
 <body>
     <div class="container"><br>
         <div class="col-md-4 col-md-offset-4">
-            <h2 class="text-center"><br>Aplikasi Sistem HCM Yayasan Kesehatan Telkom</h3>
+        <h2 class="text-center page-title"><br>Aplikasi Sistem HCM Yayasan Kesehatan Telkom</h2>
             <hr>
             @if(session('error'))
             <div class="alert alert-danger">
@@ -27,7 +74,7 @@
                     <label>Password</label>
                     <input type="password" name="password" class="form-control" placeholder="Password" required="">
                 </div>
-                <button type="submit" class="btn btn-primary btn-block">Log In</button>
+                <button type="submit" class="btn btn-primary btn-block custom-btn">Log In</button>
                 <hr>
                 <p class="text-center">Belum punya akun? <a href="/register">Register</a> sekarang!</p>
             </form>
