f5ea4a645355acd6c75ddfe2dd2e99e8e40d1235
lifiaa298 <quaaxy@gmail.com>
2025-07-03 11:33:17 +0700

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'hcm', 'employee', 'bod']);
             $table->rememberToken();
             $table->timestamps();
         });
