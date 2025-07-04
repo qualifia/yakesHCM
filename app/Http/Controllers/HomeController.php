e4fd3cc529db0bf8d2ec281e3459260d0b818090
lifiaa298 <quaaxy@gmail.com>
2025-07-04 17:02:03 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,11 +6,6 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth'); // <<< tambahkan ini
-    }
-    
     public function index() {
         return view('home'); // pastikan ada resources/views/home.blade.php
     }
