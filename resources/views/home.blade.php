d35a2d2c05baff252b6c8a6ebe613117950d943b
lifiaa298 <quaaxy@gmail.com>
2025-07-04 15:55:06 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,22 @@
-@extends('login')
-
-@section('dashboard')
-    <h4>Selamat Datang <b>{{ Auth::user()->name}}</b>, Anda Login sebagai <b>{{Auth::user()->role}}</b>.</h4>
-@endsection 
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Aplikasi Sistem HCM Yayasan Kesehatan Telkom
+</title>
+</head>
+<body>
+  <header>
+    <h2>Selamat Datang <b>{{ Auth::user()->username }}</b>, Anda Login sebagai <b>{{ Auth::user()->role }}</b>.</h2>
+    <nav>
+      <a href="/home">HOME</a>
+    </nav>
+  </header>
+  <hr/>
+  <br/>
+  <br/>
+  <hr/>
+  <footer>
+    <p>Copyright &copy; 2019 - 2021 <a href="https://www.ayongoding.com/">www.ayongoding.com</a></p>
+  </footer>
+</body>
+</html>
