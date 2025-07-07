522b1d1eb8830255abbb56d2a146ea39f18cff29
lifiaa298 <quaaxy@gmail.com>
2025-07-07 11:21:40 +0700

    Create UserSeeder.php

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        User::create([
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin123'), // jangan lupa di-hash
+            'role' => 'admin',
+        ]);
+
+        User::create([
+            'email' => 'hcm@example.com',
+            'password' => Hash::make('hcm123'),
+            'role' => 'hcm',
+        ]);
+    }
+}
+
