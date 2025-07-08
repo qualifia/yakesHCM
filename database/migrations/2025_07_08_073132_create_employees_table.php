a3d3ddccc4b0d562794d77a785425476bb58b98e
lifiaa298 <quaaxy@gmail.com>
2025-07-08 17:00:04 +0700

    Create 2025_07_08_073132_create_employees_table.php

---
+++ database/migrations/2025_07_08_073132_create_employees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('nik')->unique();
+            $table->string('name');
+            $table->date('tanggal_lahir');
+            $table->string('email')->unique();
+            $table->string('posisi');
+            $table->string('direktorat');
+            $table->string('status_karyawan');
+            $table->string('payslip_file')->nullable();
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
