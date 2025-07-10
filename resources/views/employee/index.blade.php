f5926f10a3b120039e329262e19914077984bbc5
lifiaa298 <quaaxy@gmail.com>
2025-07-10 16:59:37 +0700

    Update index.blade.php

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -9,6 +9,38 @@ body {
   background-color: #E6E6FA;
 }
 
+.navbar {
+  position: fixed;
+  top: 0;
+  left: 300px;
+  width: calc(100% - 300px); /* Supaya tidak menabrak sidebar */
+  background-color: #FFFFFF;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px 25px;
+  z-index: 1000;
+  box-shadow: 0 0 2px var(--grey-color-light);
+  height: 80px;
+}
+
+.left-info {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.navbar-name {
+  font-size: 13px; /* Membuat nama lebih tebal */
+  font-family: Poppins, sans-serif;
+  color: #080808;
+}
+
+.navbar-date {
+  font-size: 0.7em; /* Memperkecil ukuran email */
+  color: #2F4F4F; /* Mengubah warna email */
+}
+
 .sidebar {
   width: 300px;
   background-color: #FFFFFF;
@@ -20,9 +52,9 @@ body {
 }
 
 .sidebar ul {
-  font-family: Tahoma, sans-serif;
+  font-family: Poppins, sans-serif;
   font-size: 12px;
-  padding: 20px;
+  padding: 9px;
   list-style-type: none;
   margin: 0;
   width: 250px;
@@ -32,7 +64,7 @@ body {
 .sidebar ul li a {
   font-size: 12px;
   display: block;
-  color: #000;
+  color: #2F4F4F;
   padding: 8px 16px;
   text-decoration: none;
 }
@@ -54,7 +86,7 @@ ul li a:hover:not(.active) {
 .main {
   margin-top: 25px;
   margin-bottom: 10px;
-  color: #888;
+  color: #080808;
   font-size: 12px;
   font-weight: bold;
 }
@@ -62,7 +94,7 @@ ul li a:hover:not(.active) {
 .config {
   margin-top: 25px;
   margin-bottom: 10px;
-  color: #888;
+  color: #080808;
   font-size: 12px;
   font-weight: bold;
 }
@@ -71,12 +103,68 @@ ul li a:hover:not(.active) {
   margin-right: 8px;
 }
 
+.sidebar .logo {
+  display: flex; /* Mengaktifkan flexbox */
+  align-items: center; /* Menyelaraskan elemen secara vertikal */
+  gap: 20px;
+  margin-bottom: 20px;
+  border-bottom: 1px solid #A9A9A9;
+  padding: 9px;
+}
+
+.sidebar .logo img {
+  width: 45px; /* Atur lebar foto */
+  height: 45px; /* Atur tinggi foto */
+  border-radius: 10%;
+}
+
+.logo-info {
+  display: flex;
+}
+
+.logo-name {
+  font-size: 16px; 
+  font-weight: bolder;
+  font-family: Poppins, sans-serif;
+  color: #2F4F4F;
+}
+
+.sidebar .profile {
+  display: flex; /* Mengaktifkan flexbox */
+  align-items: center; /* Menyelaraskan elemen secara vertikal */
+  gap: 20px;
+  margin-bottom: 10px;
+  border-bottom: 1px solid #A9A9A9;
+  padding: 9px;
+}
+
+.sidebar .profile img {
+  width: 45px; /* Atur lebar foto */
+  height: 45px; /* Atur tinggi foto */
+  border-radius: 10%;
+}
+
+.profile-info {
+  display: flex;
+  flex-direction: column; /* Mengatur nama dan email menjadi kolom */
+}
+
+.profile-name {
+  font-size: 13px; /* Membuat nama lebih tebal */
+  font-family: Poppins, sans-serif;
+  color: #080808;
+}
+
+.profile-email {
+  font-size: 0.7em; /* Memperkecil ukuran email */
+  color: #2F4F4F; /* Mengubah warna email */
+}
+
 /* TALENT MANAGEMENT STYLES */
 #customers {
-  font-family: Tahoma, sans-serif;
+  font-family: Poppins, sans-serif;  
   border-collapse: collapse;
   width: 100%;
-
 }
 
 #customers td, #customers th {
@@ -95,8 +183,7 @@ ul li a:hover:not(.active) {
   padding-right: 12px;
   text-align: left;
   background-color: #E6E6FA;
-  color: #2F4F4F;
-
+  color: #080808;
 }
 
 #customers td {
@@ -139,9 +226,10 @@ ul li a:hover:not(.active) {
 
 .page-title {
     font-size: 20px;
-    font-weight: 700;
     margin-bottom: 20px;
     margin-top: 10px;
+    font-family: Poppins, sans-serif;
+    font-weight: bolder;
 }
 .first, .sec, .third {
     display: inline-block;
@@ -170,30 +258,53 @@ ul li a:hover:not(.active) {
 .content-area {
     background-color: white;
     padding: 40px;
-    padding-top: 50px;
-    height: 800px;
-    min-height: 100vh;
-    margin-left: 250px;
+    padding-top: 40px;
+    margin-left: 260px;
     margin-right: 1px;
     border-radius: 20px;
     width: 1100px;
-
+    margin-top: 140px;
+    min-height: calc(100vh - 100px); /* Biar tetap tinggi meski tanpa isi */
+    position: relative;
 }
 </style>
 
+<div class="navbar">
+    <div class="left-info">
+        <div class="navbar-name" >Hello, Satria Hadi!</div>
+        <div class="navbar-date">{{ \Carbon\Carbon::now()->translatedFormat('l, d F Y') }}</div>
+    </div>
+</div>
+
+
 <!-- SIDEBAR -->
 <div class="sidebar">
-  <ul class="menu">
-    <h1 class="main">Main Menu</h1>
-    <li><a href="#wp"><i class="fas fa-computer"></i>Workforce Performance</a></li>
-    <li><a class="active" href="#tlm"><i class="fas fa-users"></i>Talent Management</a></li>
-    <li><a href="#rm"><i class="fas fa-user"></i>Recruitment Management</a></li>
-    <li><a href="#trm">Training Management</a></li>
-    <li><a href="djm">DJM Management</a></li>
-    <h2 class="config">Configuration</h2>
-    <li><a href="#user">User</a></li>
-    <li><a href="#role">Role</a></li>
-  </ul>
+  <div class="logo">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/f/ff/Solid_blue.svg" alt="logo-picture">
+    <div class="logo-info">
+      <div class="logo-name">HRIS Yakes</div>
+    </div>
+  </div>
+  <div class="profile">
+    <img src="https://cdn0-production-images-kly.akamaized.net/-1pYFvsXgXGWz-U3Ybxqnc_mDfk=/1280x1706/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/5202248/original/037743300_1745894554-ChatGPT_Image_Apr_29__2025__09_26_56_AM.jpg" alt="profile_picture">
+    <div class="profile-info">
+      <div class="profile-name">Satria Hadi</div>
+      <div class="profile-email">879002@mail.com</div>
+    </div>
+  </div>
+  <nav>
+    <ul class="menu">
+      <h1 class="main">Main Menu</h1>
+      <li><a href="#wp"><i class="fas fa-computer"></i>Workforce Performance</a></li>
+      <li><a class="active" href="#tlm"><i class="fas fa-users"></i>Talent Management</a></li>
+      <li><a href="#rm"><i class="fas fa-user"></i>Recruitment Management</a></li>
+      <li><a href="#trm"><i class="fas fa-chart-line"></i>Training Management</a></li>
+      <li><a href="#djm"><i class="fas fa-folder"></i>DJM Management</a></li>
+      <h2 class="config">Configuration</h2>
+      <li><a href="#user"><i class="fas fa-user"></i>User</a></li>
+      <li><a href="#role"><i class="fas fa-gear"></i>Role</a></li>
+    </ul>
+  </nav>
 </div>
 
 
