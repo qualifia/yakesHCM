0fa32610aea20b3b8550e3a2be6ced8322c4abc8
lifiaa298 <quaaxy@gmail.com>
2025-07-24 08:51:46 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DJMController;
 use App\Http\Controllers\TMController;
+use App\Http\Controllers\FeedbackController;
+use App\Http\Controllers\TrainingController;
+
 
 
 /*
@@ -40,6 +43,10 @@ Route::middleware(['auth'])->group(function () {
 Route::get('/', [LoginController::class, 'login'])->middleware('guest')->name('login');
 Route::post('/actionlogin', [LoginController::class, 'actionLogin'])->name('actionlogin');
 Route::post('/logout', [LoginController::class, 'actionLogout'])->name('logout');
+Route::get('/employees/payslip/download/{filename}', [TMController::class, 'downloadPayslip'])->name('employees.payslip.download');
+
+Route::post('/feedback', [FeedbackController::class, 'store'])->name('feedback.store');
+
 
 Route::middleware('auth')->get('home', function () {
     return view('home');
@@ -51,12 +58,36 @@ Route::get('/home', function () {
 })->middleware('auth')->name('home');
 
 Route::get('/employees', [TMController::class, 'index'])->name('employees.index');
+
+/*
 Route::get('/employees/{employee}', [TMController::class, 'show'])->name('employees.show');
 Route::get('/employees/{id}/edit', [TMController::class, 'edit'])->name('employees.edit');
 Route::put('/employees/{id}', [TMController::class, 'update'])->name('employees.update');
 Route::get('/employees/{id}/download-payslip', [TMController::class, 'downloadPayslip'])->name('employees.downloadPayslip');
+*/
+
+Route::get('/employees', [TMController::class, 'index'])->name('employees.index');
+Route::get('/employees/{employee}', [TMController::class, 'show'])->name('employees.show');
+Route::get('/employees/{employee}/edit', [TMController::class, 'edit'])->name('employees.edit');
+Route::put('/employees/{employee}', [TMController::class, 'update'])->name('employees.update');
 
 
-Route::resource('employees', TMController::class);
+/* Route::resource('employees', TMController::class); */
 
 
+Route::get('/training', [TrainingController::class, 'index'])->name('training.index');
+
+
+
+Route::get('/djm', [DJMController::class, 'index'])->name('djm.index');
+Route::get('/djm/create', [DJMController::class, 'create'])->name('djm.create');
+Route::get('/djm/export_excel', [DJMController::class, 'exportExcel'])->name('djm.export_excel');
+Route::post('/djm', [DJMController::class, 'store'])->name('djm.store');
+Route::get('/djm/{id}', [DJMController::class, 'show'])->name('djm.show');
+Route::get('/djm/{id}/edit', [DJMController::class, 'edit'])->name('djm.edit');
+Route::get('/djm/{id}/destroy', [DJMController::class, 'destroy'])->name('djm.destroy');
+
+/*
+Route::resource('djm', DJMController::class);
+Route::get('/djm/export_excel', [DJMController::class, 'exportExcel'])->name('djm.export_excel');
+*/
