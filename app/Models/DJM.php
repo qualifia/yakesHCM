a671610cd47467400e93777f7f1e06b5b14af7c3
lifiaa298 <quaaxy@gmail.com>
2025-07-24 08:51:00 +0700

    Update DJM.php

--- app/Models/DJM.php
+++ app/Models/DJM.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DJM extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'd_j_m_s';
+    protected $fillable = [
+        'namaPosisi',
+        'regionalDirektorat',
+        'unitSub',
+        'supervisor',
+        'posisi',
+        'kodeDJM',
+        'position_specification',
+        'position_objective',
+        'responsibilities',
+        'success_indicators',
+    ]; 
 }
