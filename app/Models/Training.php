a2b28b6950ab027fa51bf924bfccaf7ef2b13059
lifiaa298 <quaaxy@gmail.com>
2025-07-29 08:33:06 +0700

    Update Training.php

--- app/Models/Training.php
+++ app/Models/Training.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Training extends Model
 {
-    //
+    use HasFactory;
+
+    public $timestamps = true;
+    
+    protected $fillable = [
+        'id_training',
+        'nama_training',
+        'deskripsi_training',
+        'tipe_training',
+        'penyelenggara',
+        'durasi',
+        'tanggal_mulai',
+        'tanggal_selesai',
+        'lokasi',
+        'metode_pelatihan',
+        'partisipan',
+        'status',
+        'biaya',
+        'total_biaya',
+    ];
 }
