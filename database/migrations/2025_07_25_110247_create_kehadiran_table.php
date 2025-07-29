4d808b6761e1a09e4cffe30f01cfe81ccf8a2b3f
lifiaa298 <quaaxy@gmail.com>
2025-07-29 08:33:13 +0700

    Create 2025_07_25_110247_create_kehadiran_table.php

---
+++ database/migrations/2025_07_25_110247_create_kehadiran_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kehadiran', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('q1');
+            $table->tinyInteger('q2');
+            $table->tinyInteger('q3');
+            $table->tinyInteger('q4');
+            $table->tinyInteger('q5');
+            $table->text('saran');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kehadiran');
+    }
+};
